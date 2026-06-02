cbef432014702c186f666eb5ed1da9820186b9e3
sheep <samcoxe0307@gmail.com>
2026-06-02 16:52:32 +0700

    feature/build_simple_calculator_application

--- default_react_js/src/App.jsx
+++ default_react_js/src/App.jsx
@@ -1,17 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import Home from './components/Home'
-import './App.css'
 
 function App() {
- 
+
+  const [firstNumber, setFirstNumber] = useState("")
+  const [secondNumber, setSecondNumber] = useState("")
+  const [result, setResult] = useState(0)
+
+  const add = () => {
+    setResult(Number(firstNumber) + Number(secondNumber))
+  }
+
+  const subtract = () => {
+    setResult(Number(firstNumber) - Number(secondNumber))
+  }
+
+  const multiply = () => {
+    setResult(Number(firstNumber) * Number(secondNumber))
+  }
+
+  const divide = () => {
+    setResult(Number(firstNumber) / Number(secondNumber))
+  }
 
   return (
-    <>
-      
-    </>
+    <div>
+
+      <h1>Simple Calculator</h1>
+
+      <input
+        type="number"
+        placeholder="First number"
+        value={firstNumber}
+        onChange={(e) => setFirstNumber(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="number"
+        placeholder="Second number"
+        value={secondNumber}
+        onChange={(e) => setSecondNumber(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={add}>+</button>
+
+      <button onClick={subtract}>-</button>
+
+      <button onClick={multiply}>*</button>
+
+      <button onClick={divide}>/</button>
+
+      <h2>Result: {result}</h2>
+
+    </div>
   )
 }
 
