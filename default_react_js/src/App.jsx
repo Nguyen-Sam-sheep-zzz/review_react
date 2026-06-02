e831500a88c162809212536852231dd322cd8617
sheep <samcoxe0307@gmail.com>
2026-06-02 15:49:27 +0700

    Bai tap function component

--- default_react_js/src/App.jsx
+++ default_react_js/src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddComponent from './components/AddComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <div className="App">
+      <AddComponent firstNumber={10} secondNumber={10} />
+     </div>
     </>
   )
 }
---
+++ default_react_js/src/components/AddComponent.jsx
@@ -0,0 +1,11 @@
+
+function AddComponent(props) {
+  return (
+    <div>
+      <h1>Add Component</h1>
+      <p>Total: {props.firstNumber + props.secondNumber}</p>
+    </div>
+  );
+}
+
+export default AddComponent;
