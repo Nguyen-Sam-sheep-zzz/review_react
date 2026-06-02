5ae3aae0077d17220cf03f0733a14f745b7ca110
sheep <samcoxe0307@gmail.com>
2026-06-02 16:06:41 +0700

    complete the exercise login/logout click

--- default_react_js/src/App.jsx
+++ default_react_js/src/App.jsx
@@ -2,14 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const handleLogin = () => {
+    setIsLoggedIn(true)
+  }
+
+  const handleLogOut = () => {
+    setIsLoggedIn(false)
+  }
 
   return (
     <>
-     
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+        {isLoggedIn ? (
+          <Home onLogOut={handleLogOut} />
+        ) : (
+          <div>
+            <h1>Welcome to the Login Page</h1>
+            <button onClick={handleLogin}>Login</button>
+          </div>
+
+        )}
+      </div>
     </>
   )
 }
