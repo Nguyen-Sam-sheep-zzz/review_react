1da76fa069ae6f00de54d8d0e959d023da279549
sheep <samcoxe0307@gmail.com>
2026-06-03 15:15:43 +0700

    complete exercise to do app

--- default_react_js/src/App.jsx
+++ default_react_js/src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import Home from './components/Home'
 import './App.css'
 
 function App() {
- 
+
+  const [task, setTask] = useState('')
+
+  const [todos, setTodos] = useState([])
+
+  const handleAdd = () => {
+
+    if (task.trim() === '') {
+      alert('Please enter a task')
+      return
+    }
+    setTodos([...todos, task])
+    setTask('')
+  }
+
 
   return (
     <>
-      
+      <h1 className='text-center'>Welcome to the todo app</h1>
+      <input type="text" placeholder='your todo' className='form-control w-25 mx-auto mb-3' value={task} onChange={(e) => setTask(e.target.value)} />
+      <button className='btn btn-primary d-block mx-auto' onClick={(handleAdd)}>
+        Add todo
+      </button>
+
+      <ul className='list-group w-25 mx-auto mt-3'>
+        {todos.map((todo, index) => (
+          <li key={index} className='list-group-item'>{todo}</li>
+        ))}
+      </ul>
     </>
   )
 }
