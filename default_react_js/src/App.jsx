b1f5b03209ba115489146ad646c02acaaf362af3
sheep <samcoxe0307@gmail.com>
2026-06-04 16:33:23 +0700

    set up

--- default_react_js/src/App.jsx
+++ default_react_js/src/App.jsx
@@ -2,30 +2,6 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(10)
-
-  useEffect(() => {
-
-    const timer = setInterval(() => {
-      setCount((prev) => prev - 1);
-    }, 1000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (count === 0) {
-      alert('Time up!');
-    }
-  }, [count]);
-
-  return (
-    <div>
-      <h1>{count}</h1>
-    </div>
-  );
 }
 
 
