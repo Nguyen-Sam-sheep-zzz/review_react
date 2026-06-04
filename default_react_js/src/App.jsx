25b26a22d0271c0f23eb6a5dcfb8abad9da0b4f5
sheep <samcoxe0307@gmail.com>
2026-06-04 15:56:14 +0700

    complete exercise

--- default_react_js/src/App.jsx
+++ default_react_js/src/App.jsx
@@ -2,11 +2,44 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+
+  const [car, setCar] = useState("Mercedes")
+  const [color, setColor] = useState("red")
 
   return (
     <>
-      
+      <h1>Select ur car</h1>
+      <label htmlFor="car">Car: </label>
+
+      <select value={car} onChange={(e) => setCar(e.target.value)}>
+        <option value={"Mercedes"}>
+          Mercedes
+        </option>
+        <option value={"BMW"}>
+          BMW
+        </option>
+        <option value={"Audi"}>
+          Audi
+        </option>
+      </select>
+
+      <br />
+      <br />
+
+      <label htmlFor="color">Color: </label>
+
+      <select
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      >
+        <option value="Black">Black</option>
+        <option value="White">White</option>
+        <option value="Red">Red</option>
+      </select>
+
+      <h3>
+        You selected a {color} - {car}
+      </h3>
     </>
   )
 }
