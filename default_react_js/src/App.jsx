a44a472784fda432f6a1219fd2c70b537243e1ce
sheep <samcoxe0307@gmail.com>
2026-06-04 16:25:08 +0700

    complete exercise

--- default_react_js/src/App.jsx
+++ default_react_js/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
- 
+  const [count, setCount] = useState(10)
+
+  useEffect(() => {
+
+    const timer = setInterval(() => {
+      setCount((prev) => prev - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (count === 0) {
+      alert('Time up!');
+    }
+  }, [count]);
 
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <h1>{count}</h1>
+    </div>
+  );
 }
+
 
 export default App
