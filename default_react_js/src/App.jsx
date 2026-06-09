aaeb9cf8f0bcda369039e13c1c363b2797246467
sheep <samcoxe0307@gmail.com>
2026-06-09 14:46:58 +0700

    complete exercise

--- default_react_js/src/App.jsx
+++ default_react_js/src/App.jsx
@@ -1,9 +1,73 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-    <>
-    </>
+    const [form, setForm] = useState({})
+
+    const handleChange = (event) => {
+        setForm({
+            ...form, [event.target.name]: event.target.value,
+        })
+    }
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault()
+
+        const isValid = form.username && form.password && form.confirmPassword
+
+        alert(
+            isValid ? "Sign up success !!!" : "Please fill out all the fields !!! "
+        );
+    };
+
+
+    return (
+        <>
+            <div style={{ maxWidth: 400, margin: "0 auto", padding: 20 }}>
+                <h1>Sign up</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="custom-input">
+                        <label>Username</label>
+                        <input
+                            name="username"
+                            value={form.username || ""}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="custom-input">
+                        <label>Email</label>
+                        <input
+                            name="email"
+                            value={form.email || ""}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="custom-input">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            value={form.password || ""}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="custom-input">
+                        <label>Confirm Password</label>
+                        <input
+                            type="password"
+                            name="confirmPassword"
+                            value={form.confirmPassword || ""}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <button type="submit">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </>
+    )
 }
 
 
