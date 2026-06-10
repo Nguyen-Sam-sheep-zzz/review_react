b5006d3bdd294cc12798ce798575eb48ee90c8ef
sheep <samcoxe0307@gmail.com>
2026-06-10 08:32:05 +0700

    update

--- default_react_js/src/App.jsx
+++ default_react_js/src/App.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Category from "./components/Category";
-import Product from "./components/Product";
+import ContactForm from "./components/ContactForm";
 
 function App() {
     return (
-        <Routes>
-            {/* Trang chính chứa dropdown chọn loại xe */}
-            <Route path="/" element={<Category />} />
-
-            {/* Trang hiển thị chi tiết sản phẩm theo categoryId */}
-            <Route path="/product/:categoryId" element={<Product />} />
-        </Routes>
+     <>
+     </>
     );
 }
 
---
+++ default_react_js/src/components/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div style={styles.container}>
+            <h1 style={styles.title}>🏠 Welcome to the Home Page</h1>
+            <p style={styles.text}>
+                This is the main landing page of our website. You can navigate to About Us or Blog from the menu above.
+            </p>
+        </div>
+    );
+}
